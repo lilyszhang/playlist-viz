f8fee1870562e06896eaf96b67e054ce58a1a0da
Lily Zhang <lilyszhang96@gmail.com>
2018-05-06 18:28:30 -0500

    updated docs

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-![Final Product](https://i.imgur.com/8pNgXlc.png)
+![Final Product Features](https://i.imgur.com/8pNgXlc.png)
+![Final Product Tempos](https://i.imgur.com/ez4Ijag.png)
 
 # Playlist Visualization Tutorial
 
@@ -53,3 +54,25 @@ for k,v in feature_ids.items():
         sum += val
     avgs[k] = sum/len(v)
 ```
+
+### Visualizing the data
+
+1. Create a new file `index.html` and set up the header with a page title and import ChartJS using this link as the source: `https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js`
+![Step 1](https://i.imgur.com/pTWFAYf.png)
+
+2. In the body tags, add in the following lines:
+```python
+<body>
+    <canvas id="songFeatures"></canvas>
+    <canvas id="songTempos"></canvas>
+</body>```
+
+3. Inside script tags, we will build our graphs. Start by creating an `averages` dictionary with the values you calculated in `tinker.py`. An example with our playlist is shown below:
+```python
+var averages = {"danceability": 67.21851851851851, "energy": 64.74814814814815, "speechiness": 7.4185185185185185, "acousticness": 19.46912592592593, "liveness": 20.227407407407412, "valence": 49.025925925925925}```
+
+4. In a polarAreaChart variable, follow the structure on the [ChartJS documentation](http://www.chartjs.org/docs/latest/#creating-a-chart). This will create your first graph, showing the average feature value of songs in your selected playlist for each of the labels in the chart.
+![Step 4](https://i.imgur.com/pGciqgm.png)
+
+5. To create our tempo graph, create a variable titled barChart and set the labels to be an **array of song titles** in your playlist. Set the dataset to be the **array of tempos of the songs**.
+![Step 5](https://i.imgur.com/dafFyRv.png)
--- tinker.py
+++ tinker.py
@@ -8,47 +8,33 @@ with open('songs.json') as json_data:
         song_ids.append(item['track']['id'])
         song_names.append(item['track']['name'])
 
-#print(song_ids)
+print('Song IDs: ' + str(song_ids))
 
 feature_ids = {}
 tempo = []
 with open('features.json') as json_data:
     f = json.load(json_data)
-for feature in f['audio_features']:
-    feature_ids['danceability'] = feature_ids.get('danceability',[])
-    feature_ids['danceability'].append(feature['danceability']*100)
-
-    feature_ids['energy'] = feature_ids.get('energy',[])
-    feature_ids['energy'].append(feature['energy']*100)
-
-    feature_ids['speechiness'] = feature_ids.get('speechiness',[])
-    feature_ids['speechiness'].append(feature['speechiness']*100)
-
-    feature_ids['acousticness'] = feature_ids.get('acousticness',[])
-    feature_ids['acousticness'].append(feature['acousticness']*100)
-
-    feature_ids['liveness'] = feature_ids.get('liveness',[])
-    feature_ids['liveness'].append(feature['liveness']*100)
-
-    feature_ids['valence'] = feature_ids.get('valence',[])
-    feature_ids['valence'].append(feature['valence']*100)
-
-    tempo.append(feature['tempo'])
+features = ['danceability','energy','speechiness','acousticness','liveness','valence']
+for song in f['audio_features']:
+    for feature in features:
+        feature_ids[feature] = feature_ids.get(feature,[])
+        feature_ids[feature].append(song[feature]*100)
+    tempo.append(song['tempo'])
 
 songs = {}
 for song in song_names:
     for t in tempo:
         songs[song] = t
-
 avgs = {}
+
 for k,v in feature_ids.items():
     sum = 0
     for val in v:
         sum += val
     avgs[k] = sum/len(v)
 
-print (songs.keys())
+print('Song Titles: ' + str(songs.keys()) + '\n')
+print('Feature Averages: ' + str(avgs) + '\n')
+
 with open('avgs.json', 'w') as fp:
     json.dump(avgs, fp)
-
-# danceability, energy, speechiness, acousticness, instrumentalness, liveness, valence
